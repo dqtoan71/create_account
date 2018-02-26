9d735a8cdf32df02df42adc7dcbdf849ce614256
ToanDang <dqtoan71@gmail.com>
2018-02-26 17:14:06 +0700

    yet

--- src/account.php
+++ src/account.php
@@ -38,15 +38,13 @@ class account
 
         if (!empty($param['username']) && !empty($param['password']) && !empty($param['email'])) {
 
-            return true;
-
             $this->username = $this->db->real_escape_string($param['username']);
             $this->password = sha1($this->db->real_escape_string($param['password']));
             $this->email = $this->db->real_escape_string($param['email']);
 
             $query  = 'INSERT INTO users (user, password, email) '
                 . 'VALUES ("' . $this->username . '", "' . $this->password . '", "' . $this->email . '")';
-
+            return $query;
             return $this->db->query($query);
 
         }
