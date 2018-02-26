9bccb846940058391d4bd13721245ebfe4e5fd43
ToanDang <dqtoan71@gmail.com>
2018-02-26 14:39:03 +0700

    yet

--- src/account.php
+++ src/account.php
@@ -1,6 +1,15 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 2/26/2018
+ * Time: 2:38 PM
+ */
+
 namespace create_account\create_account;
-class create_account
+
+
+class account
 {
     public static function world()
     {
